276ea76f4964bf8b298d5e08e48d42b410975f12
김종민 <104364725+kimjongk@users.noreply.github.com>
2025-05-25 18:07:22 +0900

    Update main.js

--- Seoul city hall/asset/js/main.js
+++ Seoul city hall/asset/js/main.js
@@ -1,5 +1,14 @@
     
     /**
+     * alert modal
+     */
+    const alertModal = document.querySelector('#alertModal');
+    const modalClose = document.querySelector('#alertModal .closeBtn');
+    modalClose.addEventListener('click',()=>{
+        alertModal.style.display = 'none';
+    })
+
+    /**
      * 언어선택
      */
 
