293df599137def1e06c71a92b0754e325c6c8b97
김종민 <104364725+kimjongk@users.noreply.github.com>
2025-05-25 18:25:02 +0900

    Update main.js

--- tvN/asset/js/main.js
+++ tvN/asset/js/main.js
@@ -1,3 +1,10 @@
+/* alert modal */
+const alertModal = document.querySelector('#alertModal');
+const modalClose = document.querySelector('#alertModal .closeBtn');
+modalClose.addEventListener('click',()=>{
+    alertModal.style.display = 'none';
+});
+
 /**
  * 메인비쥬얼 스와이퍼
  */
