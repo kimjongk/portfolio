4761b3bf812abb5c106a4f45009ada47c1f9de74
김종민 <104364725+kimjongk@users.noreply.github.com>
2025-05-25 19:15:34 +0900

    Update main.js

--- MONCOLONY/asset/js/main.js
+++ MONCOLONY/asset/js/main.js
@@ -1,3 +1,10 @@
+/* alert modal */
+const alertModal = document.querySelector('#alertModal');
+const modalClose = document.querySelector('#alertModal .closeBtn');
+modalClose.addEventListener('click',()=>{
+    alertModal.style.display = 'none';
+});
+
 const lenis = new Lenis()
 
 lenis.on('scroll', ScrollTrigger.update)
